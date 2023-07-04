a56a5dbd57edb80a46c2305bc22b6820f89915eb
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2023-07-04 22:26:55 +0100

    add league creation, result add and admin update function

--- saga/examples/create_result.exs
+++ saga/examples/create_result.exs
@@ -1,19 +1,32 @@
 # run this while in the 'saga' project root:
 #
-# >iex --dot-iex examples/create_result.exs -S mix
+# > iex --dot-iex examples/create_result.exs -S mix
 #
 # it will open an iex shell with the script outcome computed, and variables available
 #
 # Otherwise, running the follwoing will exec the script:
 #
-# >mix run examples/create_result.exs
+# > mix run examples/create_result.exs
 
-alias Saga.Core.{GameResult, Player}
+alias Saga.Core.{GameResult, Player, League}
 
 p1 = Player.create_player(1, "Dave")
 
 p2 = Player.create_player(2, "Lisa")
 
-result = GameResult.player2_won(DateTime.utc_now(), p1, p2)
+p3 = Player.create_player(3, "Janet")
 
-IO.puts("Result: #{inspect(result, pretty: true)}")
+league = League.new("Chess newbies", "Players with ELO < 9000", p2, [])
+
+IO.puts("League created: #{inspect(league, pretty: true)}")
+
+result1 = GameResult.player2_won(DateTime.utc_now(), p1, p2)
+result2 = GameResult.draw(DateTime.utc_now(), p1, p3)
+
+updated_league =
+  league
+  |> League.add_result(result1)
+  |> League.add_result(result2)
+  |> League.update_admin(p2)
+
+IO.puts("League updated: #{inspect(updated_league, pretty: true)}")
--- saga/lib/saga/core/league.ex
+++ saga/lib/saga/core/league.ex
@@ -9,4 +9,24 @@ defmodule Saga.Core.League do
           admin: Player.t(),
           results: list(GameResult.t())
         }
+
+  def new(name, description, %Player{} = admin, results)
+      when is_binary(name) and
+             is_binary(description) and
+             is_list(results) do
+    %__MODULE__{
+      name: name,
+      description: description,
+      admin: admin,
+      results: results
+    }
+  end
+
+  def add_result(%__MODULE__{results: results} = league, new_result) do
+    %__MODULE__{league | results: [new_result | results]}
+  end
+
+  def update_admin(%__MODULE__{} = league, %Player{} = new_admin) do
+    %__MODULE__{league | admin: new_admin}
+  end
 end
