c07b4c598e121c47b7a0b2a3a2b82fa2f5f73936
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2023-07-06 23:44:48 +0100

    replace __MODULE__ with full module names

--- saga/lib/saga/core/player_stats.ex
+++ saga/lib/saga/core/player_stats.ex
@@ -3,7 +3,7 @@ defmodule Saga.Core.PlayerStats do
   @enforce_keys ~w[name games wins losses draws opponents]a
   defstruct ~w[name games wins losses draws opponents]a
 
-  @type t :: %__MODULE__{
+  @type t :: %Saga.Core.PlayerStats{
           name: String.t(),
           games: non_neg_integer(),
           wins: non_neg_integer(),
@@ -13,7 +13,7 @@ defmodule Saga.Core.PlayerStats do
         }
 
   def no_games(name) do
-    %__MODULE__{
+    %Saga.Core.PlayerStats{
       name: name,
       games: 0,
       wins: 0,
@@ -23,16 +23,16 @@ defmodule Saga.Core.PlayerStats do
     }
   end
 
-  def increment(%__MODULE__{wins: n} = st, :win, opponent),
-    do: %__MODULE__{st | wins: n + 1} |> increment_games(opponent)
+  def increment(%Saga.Core.PlayerStats{wins: n} = st, :win, opponent),
+    do: %Saga.Core.PlayerStats{st | wins: n + 1} |> increment_games(opponent)
 
-  def increment(%__MODULE__{losses: n} = st, :loss, opponent),
-    do: %__MODULE__{st | losses: n + 1} |> increment_games(opponent)
+  def increment(%Saga.Core.PlayerStats{losses: n} = st, :loss, opponent),
+    do: %Saga.Core.PlayerStats{st | losses: n + 1} |> increment_games(opponent)
 
-  def increment(%__MODULE__{draws: n} = st, :draw, opponent),
-    do: %__MODULE__{st | draws: n + 1} |> increment_games(opponent)
+  def increment(%Saga.Core.PlayerStats{draws: n} = st, :draw, opponent),
+    do: %Saga.Core.PlayerStats{st | draws: n + 1} |> increment_games(opponent)
 
-  def increment_games(%__MODULE__{} = st, %Player{id: id, name: name}) do
+  def increment_games(%Saga.Core.PlayerStats{} = st, %Player{id: id, name: name}) do
     st
     |> Map.put(:games, st.games + 1)
     |> Map.put(:opponents, Map.put_new(st.opponents, id, name))
