c2f2355ef62c019c172264616a0ce09ce8f111e5
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2023-07-10 23:41:24 +0100

    use min/max

--- saga_web/lib/saga_web_web/live/light_live.ex
+++ saga_web/lib/saga_web_web/live/light_live.ex
@@ -44,12 +44,12 @@ defmodule SagaWebWeb.LightLive do
   end
 
   def handle_event("up", _, socket) do
-    socket = update(socket, :brightness, &(&1 + 10))
+    socket = update(socket, :brightness, &min(&1 + 10, 100))
     {:noreply, socket}
   end
 
   def handle_event("down", _, socket) do
-    socket = update(socket, :brightness, &(&1 - 10))
+    socket = update(socket, :brightness, &max(&1 - 10, 0))
     {:noreply, socket}
   end
 end
