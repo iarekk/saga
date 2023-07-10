b484ce932aefa6b4552623276fa7839007fb10f3
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2023-07-10 23:35:05 +0100

    working buttons

--- saga_web/lib/saga_web_web/live/light_live.ex
+++ saga_web/lib/saga_web_web/live/light_live.ex
@@ -14,6 +14,44 @@ defmodule SagaWebWeb.LightLive do
         <%= @brightness %>%
       </span>
     </div>
+
+    <button phx-click="off">
+      Off
+    </button>
+
+    <button phx-click="down">
+      Down
+    </button>
+
+    <button phx-click="up">
+      Up
+    </button>
+
+    <button phx-click="on">
+      On
+    </button>
     """
+  end
+
+  def handle_event("on", _, socket) do
+    socket = assign(socket, :brightness, 100)
+    {:noreply, socket}
+  end
+
+  def handle_event("off", _, socket) do
+    socket = assign(socket, :brightness, 0)
+    {:noreply, socket}
+  end
+
+  def handle_event("up", _, socket) do
+    new_brightness = socket.assigns.brightness + 10
+    socket = assign(socket, :brightness, new_brightness)
+    {:noreply, socket}
+  end
+
+  def handle_event("down", _, socket) do
+    new_brightness = socket.assigns.brightness - 10
+    socket = assign(socket, :brightness, new_brightness)
+    {:noreply, socket}
   end
 end
