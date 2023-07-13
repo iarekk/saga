3e3069a45c10575c9493a1afc439080068dded9e
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2023-07-14 00:34:07 +0100

    add css and grid

--- saga_web/lib/saga_web_web/live/league_live.ex
+++ saga_web/lib/saga_web_web/live/league_live.ex
@@ -2,30 +2,8 @@ defmodule SagaWebWeb.LeagueLive do
   use SagaWebWeb, :live_view
   alias Saga.Core.GameResult
 
-  def mount(_, _, socket) do
+  def mount(_params, _session, socket) do
     {:ok, socket |> add_league()}
-  end
-
-  def add_league(socket) do
-    league = Saga.Core.MockProvider.sample_league()
-
-    socket |> assign(:league_name, league.name)
-    |> assign(:league_description, league.description)
-    |> assign(:admin_name, league.admin.name)
-    |> assign(:games, prep_results(league.results))
-  end
-
-  def prep_results(results) do
-    results
-    |> Enum.sort_by(&(&1.date))
-    |> Enum.map(&add_player_outcomes/1)
-  end
-
-  def add_player_outcomes(game_result) do
-    {player1_outcome, player2_outcome} = GameResult.player_outcomes(game_result.outcome)
-    game_result
-    |> Map.put(:player1_outcome, player1_outcome)
-    |> Map.put(:player2_outcome, player2_outcome)
   end
 
   def render(assigns) do
@@ -35,26 +13,43 @@ defmodule SagaWebWeb.LeagueLive do
       <h2>Description: <%= @league_description %></h2>
       <h3>Admin: <%= @admin_name %></h3>
 
-      <hr/>
-
-      Games played:<br/>
-      <ol type="1">
-      <%= for game <- @games do %>
-        <li>
-          <div class="game-result">
-            <span class={"#{game.player1_outcome}"}>
-              <%= game.player1.name %>
-            </span>
-            vs.
-            <span class={"#{game.player2_outcome}"}>
-              <%= game.player2.name %>
-            </span>
-            on: <%= game.date %>
+      <hr /> Games played:<br />
+      <div class="games-grid">
+        <%= for game <- @games do %>
+          <div class={"player1 #{game.player1_outcome}"}>
+            <%= game.player1.name %>
           </div>
-        </li>
-      <% end %>
-      </ol>
+          <div class={"player2 #{game.player2_outcome}"}>
+            <%= game.player2.name %>
+          </div>
+          <div class="game-time">on: <%= game.date %></div>
+        <% end %>
+      </div>
     </div>
     """
+  end
+
+  def add_league(socket) do
+    league = Saga.Core.MockProvider.sample_league()
+
+    socket
+    |> assign(:league_name, league.name)
+    |> assign(:league_description, league.description)
+    |> assign(:admin_name, league.admin.name)
+    |> assign(:games, prep_results(league.results))
+  end
+
+  def prep_results(results) do
+    results
+    |> Enum.sort_by(& &1.date)
+    |> Enum.map(&add_player_outcomes/1)
+  end
+
+  def add_player_outcomes(game_result) do
+    {player1_outcome, player2_outcome} = GameResult.player_outcomes(game_result.outcome)
+
+    game_result
+    |> Map.put(:player1_outcome, player1_outcome)
+    |> Map.put(:player2_outcome, player2_outcome)
   end
 end
