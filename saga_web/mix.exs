6c41ca0e00fbdc66ea2789c4541d368b2d081b2a
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2023-07-16 19:25:33 +0100

    add credo

--- saga_web/mix.exs
+++ saga_web/mix.exs
@@ -47,6 +47,7 @@ defmodule SagaWeb.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:saga, path: "../saga"}
     ]
   end
